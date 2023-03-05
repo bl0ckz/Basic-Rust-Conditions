a93a9653def940cf739735112e3bd2bb9e4cf79c
bl0ckz <bl0ckz@i2pmail.org>
2023-03-05 00:54:32 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+
+    let food = "cookie";
+    if foog == "cookie" {
+        println!("I like cookies too");
+    } else if food == "fruit" {
+        println!("That sounds healthy")
+    } else {
+        println!("oh, that's to bad")
+    }
+}
